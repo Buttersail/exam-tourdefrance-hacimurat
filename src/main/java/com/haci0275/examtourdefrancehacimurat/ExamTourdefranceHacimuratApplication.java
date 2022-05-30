798414de0710e5460cc2b0575bc67cf95f4a2585
Murat Memis <3605716+Buttersail@users.noreply.github.com>
2022-05-30 11:21:21 +0200

    initial commit

---
+++ src/main/java/com/haci0275/examtourdefrancehacimurat/ExamTourdefranceHacimuratApplication.java
@@ -0,0 +1,13 @@
+package com.haci0275.examtourdefrancehacimurat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamTourdefranceHacimuratApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamTourdefranceHacimuratApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haci0275/examtourdefrancehacimurat/ExamTourdefranceHacimuratApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haci0275.examtourdefrancehacimurat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamTourdefranceHacimuratApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
